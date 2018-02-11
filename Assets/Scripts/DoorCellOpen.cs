0f2cefb320a36ad286565fb288017525fb17f338
Ziarqq <ziarko.ka@gmail.com>
2018-02-11 17:36:34 +0100

    Added folders/healthBar/Player/Door

---
+++ Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour {
+
+    public float Distance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject TheDoor;
+    public AudioSource Sound;
+
+	void Update ()
+    {
+        Distance = PlayerCasting.DistanceFromTarget;
+	}
+
+    void OnMouseOver()
+    {
+        if(Distance <= 2)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if(Distance > 2)
+        {
+            ActionDisplay.SetActive(false);
+            ActionText.SetActive(false);
+        }
+        if(Input.GetButtonDown("Action"))
+        {
+            if (Distance <= 2)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                TheDoor.GetComponent<Animation>().Play("DoorOpenAnim");
+                Sound.Play();
+            }
+        }
+    }
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
