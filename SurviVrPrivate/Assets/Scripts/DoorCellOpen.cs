db4121ed7834feeb36617ecfd1cacbb77158532d
arthurlaunoy <arthur.launoy@epita.fr>
2018-02-15 21:16:28 +0100

    Title

--- SurviVrPrivate/Assets/Scripts/DoorCellOpen.cs
+++ SurviVrPrivate/Assets/Scripts/DoorCellOpen.cs
@@ -11,42 +11,53 @@ public class DoorCellOpen : MonoBehaviour {
     public GameObject TheDoor;
     public GameObject ExtraCrosshair;
     public AudioSource Sound;
+    public Animation anim;
 
 	void Update ()
     {
+        anim = TheDoor.GetComponent<Animation>();
         Distance = PlayerCasting.DistanceFromTarget;
 	}
 
     void OnMouseOver()
     {
-        if(Distance <= 2)
+        if (Distance <= 2)
         {
             ExtraCrosshair.SetActive(true);
             ActionDisplay.SetActive(true);
             ActionText.SetActive(true);
         }
-        if(Distance > 2)
+        if (Distance > 2)
         {
             ExtraCrosshair.SetActive(false);
             ActionDisplay.SetActive(false);
             ActionText.SetActive(false);
         }
-        if(Input.GetButtonDown("Action"))
+        if (Input.GetButtonDown("Action"))
         {
             if (Distance <= 2)
             {
                 this.GetComponent<BoxCollider>().enabled = false;
                 ActionDisplay.SetActive(false);
                 ActionText.SetActive(false);
-                TheDoor.GetComponent<Animation>().Play("DoorOpenAnim");
+                anim.Play("DoorOpenAnim");
                 Sound.Play();
+                StartCoroutine(DoorClose());
             }
         }
+
     }
     void OnMouseExit()
     {
         ExtraCrosshair.SetActive(false);
         ActionDisplay.SetActive(false);
         ActionText.SetActive(false);
+    }
+    IEnumerator DoorClose()
+    {
+        yield return new WaitForSeconds(5);
+        anim.Play("DoorCloseAnim");
+        Sound.Play();
+        this.GetComponent<BoxCollider>().enabled = true;
     }
 }
