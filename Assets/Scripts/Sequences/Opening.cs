dad228bbf07887ab739fef84c3efd2fadfe427b4
Unknown <ziarko.ka@gmail.com>
2018-03-12 19:08:36 +0100

    Finished Opening Animation

---
+++ Assets/Scripts/Sequences/Opening.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class Opening : MonoBehaviour {
+
+    public GameObject ThePlayer;
+    public GameObject FadeScreen;
+    public GameObject TextBox;
+
+	void Start () {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+	}
+
+    IEnumerator ScenePlayer ()
+    {
+        yield return new WaitForSeconds(0.5f);
+        TextBox.GetComponent<Text>().text = "Where am I...";
+        yield return new WaitForSeconds(2f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+
+        
+    }
+	
+
+}
