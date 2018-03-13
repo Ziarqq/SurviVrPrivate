4b68cedb927621808262867b0c70148b7d9c2bae
Unknown <ziarko.ka@gmail.com>
2018-03-14 00:24:08 +0100

    Added 1 Water Drop in the toilet

---
+++ Assets/Scripts/WaterDrops.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDrops : MonoBehaviour
+{
+    public AudioSource Water;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Water.Play();
+        }
+    }
+}
