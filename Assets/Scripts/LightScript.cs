65159526acbb8aca18441f5b1872f39b0c2f938f
MartinRuhlmann <martin.ruhlmann@epita.fr>
2018-03-13 16:32:39 +0100

    It didn't work
    shit

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    public int LightMode;
+    public GameObject LampLight;
+
+	void Update () {
+		if (LightMode == 0)
+        {
+            StartCoroutine(AnimateLight());
+        }
+	}
+
+    IEnumerator AnimateLight ()
+    {
+        LightMode = Random.Range(1, 3);
+        if (LightMode == 1)
+        {
+            LampLight.GetComponent<Animation>().Play("BrokenLight1");
+        }
+        if (LightMode == 2)
+        {
+            LampLight.GetComponent<Animation>().Play("BrokenLight2");
+        }
+        yield return new WaitForSeconds(0.99f);
+        LightMode = 0;
+    }
+}
