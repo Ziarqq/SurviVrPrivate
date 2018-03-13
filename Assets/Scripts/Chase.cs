771ef6e53b90c9950f36131fa73b3114d06372f3
Unknown <ziarko.ka@gmail.com>
2018-03-13 03:59:54 +0100

    Added sky and chase script

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+
+    public Transform player;
+
+	void Start () {
+        
+	}
+	
+	void Update () {
+		
+        if(Vector3.Distance(player.position,this.transform.position) < 8)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            if(direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, 0.1f);
+
+            }
+        }
+	}
+}
