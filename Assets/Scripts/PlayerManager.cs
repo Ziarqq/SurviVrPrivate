88bd6df85f72e98ce70bb718058ed45b215b52ea
Unknown <ziarko.ka@gmail.com>
2018-03-13 18:00:21 +0100

    Added moving dmg slider on touch with zombie

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour {
+
+    int Health = 100;
+    public Slider HealthBar;
+
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            //You DIED
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Health -= 100;
+            HealthBar.value -= 0.1f;
+        }
+    }
+}
