7f2f40afbf2a9e6bf641e372f6296ad2ac7995cb
Unknown <ziarko.ka@gmail.com>
2018-04-24 18:55:43 +0200

    Obstacle/Enemy destruction
    Added 2 simple scripts that allow to kill the enemy/destroy the obstacle -> alpha version

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public GameObject Enemy;
+
+    int Health = 100;
+
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            Enemy.SetActive(false);
+            //ADD DEATH ANIMATION HERE 
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Weapon"))
+        {
+            Health -= 25;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+
+    public GameObject obstacle;
+
+    int Health = 100;
+
+    void Update()
+    {
+        if (Health <= 0)
+        {
+            obstacle.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Weapon"))
+        {
+            Health -= 50;
+        }
+    }
+}
