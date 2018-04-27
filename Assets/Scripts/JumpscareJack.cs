de163a2f3918dc3d757883c83109b1fddba6bea2
Unknown <ziarko.ka@gmail.com>
2018-04-27 18:26:48 +0200

    1st spooky thing

---
+++ Assets/Scripts/JumpscareJack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpscareJack : MonoBehaviour {
+    public AudioSource laught;
+    public AudioSource JackSong;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Jumpscare());
+    }
+
+    IEnumerator Jumpscare()
+    {
+        yield return new WaitForSeconds(2f);
+        laught.Play();
+        yield return new WaitForSeconds(3f);
+        JackSong.Play();
+    }
+}
