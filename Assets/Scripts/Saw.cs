076e7330184f68b6353524423d0304b1309a32f4
Unknown <ziarko.ka@gmail.com>
2018-04-28 17:24:03 +0200

    Morgue

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Saw : MonoBehaviour {
+
+    public GameObject MessagePanel;
+    public Text txt; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MessagePanel.SetActive(true);
+        txt.text = "- You can't pick this up -";
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        MessagePanel.SetActive(false);
+    }
+}
