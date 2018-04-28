9f089fb4e0aa7651b78aaf4043785346594d2690
Unknown <ziarko.ka@gmail.com>
2018-04-28 15:50:50 +0200

    Skeleton with working animations

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class Chase : MonoBehaviour {
 
     public Transform player;
+    public Animator anim;
 
 	void Start () {
-        
+        anim = GetComponent<Animator>();
 	}
 	
 	void Update () {
-		
-        if(Vector3.Distance(player.position,this.transform.position) < 8)
+        Vector3 direction = player.position - this.transform.position;
+
+        if (Vector3.Distance(player.position,this.transform.position) < 8)
         {
-            Vector3 direction = player.position - this.transform.position;
             direction.y = 0;
 
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
 
-            if(direction.magnitude > 5)
+            anim.SetBool("isIddle", false);
+            if(direction.magnitude > 3)
             {
                 this.transform.Translate(0, 0, 0.1f);
-
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
             }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIddle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
         }
 	}
 }
