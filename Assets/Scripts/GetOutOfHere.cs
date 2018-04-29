13fc532da34547601bec106f9bfb423c13b4fa12
Unknown <ziarko.ka@gmail.com>
2018-04-29 22:06:07 +0200

    Last Commit before presentation

---
+++ Assets/Scripts/GetOutOfHere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GetOutOfHere : MonoBehaviour {
+
+    public GameObject ThePlayer;
+    public GameObject Message;
+    public GameObject Inventory;
+    public GameObject Slider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(GetOut());
+    }
+    IEnumerator GetOut()
+    {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        Inventory.SetActive(false);
+        Slider.SetActive(false);
+        Message.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        Message.SetActive(false);
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+        gameObject.SetActive(false);
+        Slider.SetActive(true);
+        Inventory.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/TeddyBear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyBear : MonoBehaviour {
+
+    public GameObject TeddyText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TeddyText.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        TeddyText.SetActive(false);
+    }
+}
