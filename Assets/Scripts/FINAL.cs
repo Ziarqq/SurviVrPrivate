5971f86d34f6a4544a3dc05b5ebbb96f389b3dee
Unknown <ziarko.ka@gmail.com>
2018-06-05 00:18:17 +0200

    ALMOST DONE SINGLE

---
+++ Assets/Scripts/FINAL.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FINAL : MonoBehaviour {
+
+    public GameObject Car;
+    public GameObject text;
+    public static bool MadeIt = true;
+    public GameObject FinishTrigger;
+    private Text count;
+
+    private void Start()
+    {
+        count = text.GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Counter());
+        FinishTrigger.SetActive(true);
+    }
+
+    IEnumerator Counter()
+    {
+        yield return new WaitForSeconds(7f);
+        text.SetActive(true);
+        count.text = "10";
+        yield return new WaitForSeconds(1f);
+        count.text = "9";
+        yield return new WaitForSeconds(1f);
+        count.text = "8";
+        yield return new WaitForSeconds(1f);
+        count.text = "7";
+        yield return new WaitForSeconds(1f);
+        count.text = "6";
+        yield return new WaitForSeconds(1f);
+        count.text = "5";
+        yield return new WaitForSeconds(1f);
+        count.text = "4";
+        yield return new WaitForSeconds(1f);
+        count.text = "3";
+        yield return new WaitForSeconds(1f);
+        count.text = "2";
+        yield return new WaitForSeconds(1f);
+        count.text = "1";
+        yield return new WaitForSeconds(1f);
+        count.text = "THE CAR IS GONE...";
+        MadeIt = false;
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/FINISH.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.SceneManagement;
+
+public class FINISH : MonoBehaviour {
+    public Text text;
+    public GameObject FinalMessage;
+    public GameObject Player;
+    public GameObject Credits;
+    public GameObject YouFailed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (FINAL.MadeIt)
+        {
+            text.enabled = false;
+            FinalMessage.SetActive(true);
+            StartCoroutine(Finish());
+        }
+        if(!FINAL.MadeIt)
+        {
+            text.enabled = false;
+            StartCoroutine(NotFinished());
+        }
+    }
+
+    IEnumerator Finish()
+    {
+        yield return new WaitForSeconds(5f);
+        Player.GetComponent<FirstPersonController>().enabled = false;
+        FinalMessage.SetActive(false);
+        Credits.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("main-menu");
+
+    }
+
+    IEnumerator NotFinished()
+    {
+        Player.GetComponent<FirstPersonController>().enabled = false;
+        YouFailed.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        YouFailed.SetActive(false);
+        Credits.SetActive(true);
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("main-menu");
+
+    }
+
+}
